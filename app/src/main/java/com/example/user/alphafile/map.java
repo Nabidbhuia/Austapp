9ed1b6cf26214f64e8b2675184084a934343c7f7
nabid <nabidbhuia313@gmail.com>
2019-04-17 07:45:49 +0600

    commiting source code

---
+++ app/src/main/java/com/example/user/alphafile/map.java
@@ -0,0 +1,13 @@
+package com.example.user.alphafile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class map extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
